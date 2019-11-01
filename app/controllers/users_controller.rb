2f9f0e38b1486f37efeec3c78c37abb4e18578df
jucora <artjulius@hotmail.com>
2019-11-01 10:51:15 -0500

    using rails form_for

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
 
   def create
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-    #@user = User.new(user_params)
+    #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
 
     if @user.save
       redirect_to new_user_path
@@ -12,10 +14,10 @@ class UsersController < ApplicationController
     end
   end
 
-  ·#private
+  private
 
-  	#def user_params
-  	#	params.require(:user).permit(:username, :email, :password)
-  	#end
+  	def user_params
+  	  params.require(:user).permit(:username, :email, :password)
+  	end
 
 end
