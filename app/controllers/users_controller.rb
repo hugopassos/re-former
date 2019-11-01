b0aa6bb487a263827ca1ebf0a2d4c146b679b9b2
hugopassos <hugo.passos@hotmail.com>
2019-11-01 14:54:26 -0300

    Fixing stickler

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
-  
   def index; end
 
   def show
@@ -11,7 +12,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
 
     if @user.save
@@ -27,18 +28,17 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    
+
     if @user.update(user_params)
       redirect_to new_user_path
     else
       render :new
-    end 
+    end
   end
 
   private
 
-  	def user_params
-  	  params.require(:user).permit(:username, :email, :password)
-  	end
-
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
 end
